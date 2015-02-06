be8d3c290753cb5fbf4e42c6563301807266de3e
Janne Martikainen <janne.martikainen@numerola.fi>
2015-02-06 11:21:30 +0200

    Fixed the wrapper test

--- numerrin_wrapper/tests/test_numerrin_wrapper.py
+++ numerrin_wrapper/tests/test_numerrin_wrapper.py
@@ -6,27 +6,22 @@ mesh module functionalities
 """
 
 import unittest
-from foam_wrapper.model import Model
 from numerrin_wrapper.numerrin_wrapper import NumerrinPool, NumerrinCode, NumerrinWrapper
 
 class NumerrinWrapperTestCase(unittest.TestCase):
     """Test case for NumerrinWrapper class"""
     def setUp(self):
         """Creates dummy model to perform tests"""
-        self.model = Model(1)
-
         
     def test_meshRead(self):
         """Test mesh read from Numerrin to Simphony"""
-        numerrin_wrapper = NumerrinWrapper(self.model)
-        numerrin_wrapper.setProgramFile("numerrin_wrapper/tests/mesh_extrusion.num")
+        numerrin_wrapper = NumerrinWrapper()
+        numerrin_wrapper.parseProgramFile("numerrin_wrapper/tests/mesh_extrusion.num")
         numerrin_wrapper.run()
-        simphonyMesh=numerrin_wrapper.exportMesh()
-        print "Number of mesh points: ",sum(1 for _ in simphonyMesh.iter_points())
-        print "Number of mesh cells : ",sum(1 for _ in simphonyMesh.iter_cells())
-        print "Number of mesh faces : ",sum(1 for _ in simphonyMesh.iter_faces())
-
-
+        (simphonyMesh,numerrinMeshMap)=numerrin_wrapper.get_mesh("mesh")
+        assert(sum(1 for _ in simphonyMesh.iter_points()) == 9261)
+        assert(sum(1 for _ in simphonyMesh.iter_cells()) == 8000)
+        assert(sum(1 for _ in simphonyMesh.iter_faces()) == 25200)
 
 if __name__ == '__main__':
     unittest.main()
