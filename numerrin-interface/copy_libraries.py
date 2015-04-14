a4f9d46d03c21c4725d6fbf2e678950083b92fd8
Kai Hiltunen <kai.hiltunen@numerola.fi>
2015-04-14 10:57:17 +0300

    fixed

--- numerrin-interface/copy_libraries.py
+++ numerrin-interface/copy_libraries.py
@@ -1,9 +1,9 @@
-import site
+import sys
 import shutil
 import os
 
-src = ['numerrin.so', 'numerrin4.so']
-destdir = site.getsitepackages()
+src = ['numerrin.so', 'libnumerrin4.so']
+destdir = [f for f in sys.path if f.endswith('site-packages')]
 if type(destdir) is list:
     destdir = destdir[0]
 for f in src:
