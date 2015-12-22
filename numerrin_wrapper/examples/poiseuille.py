4290d23100d4a299d67adc9b1e3825d1aaecf195
Kai Hiltunen <kai.hiltunen@numerola.fi>
2015-12-22 16:26:02 +0200

    adding solvers

--- numerrin_wrapper/examples/poiseuille.py
+++ numerrin_wrapper/examples/poiseuille.py
@@ -3,21 +3,22 @@
 """
 
 from simphony.core.cuba import CUBA
-from simphony.engine import numerrin
-import os
+#from simphony.engine import numerrin
+from numerrin_wrapper import numerrin_wrapper as numerrin
+from numerrin_wrapper.mesh_utils import create_quad_mesh
 
+import os
 
 wrapper = numerrin.NumerrinWrapper()
 CUBAExt = numerrin.CUBAExt
-
 name = 'poiseuille'
 
 wrapper.CM[CUBA.NAME] = name
 
 wrapper.CM_extensions[CUBAExt.GE] = (CUBAExt.INCOMPRESSIBLE,
                                      CUBAExt.LAMINAR_MODEL)
-wrapper.SP[CUBA.TIME_STEP] = 1
-wrapper.SP[CUBA.NUMBER_OF_TIME_STEPS] = 10
+wrapper.SP[CUBA.TIME_STEP] = 0.1
+wrapper.SP[CUBA.NUMBER_OF_TIME_STEPS] = 1
 wrapper.SP[CUBA.DENSITY] = 1.0
 wrapper.SP[CUBA.DYNAMIC_VISCOSITY] = 1.0
 
@@ -34,11 +35,14 @@ wrapper.BC[CUBA.PRESSURE] = {'boundary0': 'zeroGradient',
 corner_points=((0.0,0.0), (30.0e-3,0.0), (30.0e-3,5.0e-3), (0.0,5.0e-3))
 extrude_length = 0.1e-3
 nex = 30
-ney = 4
+ney = 3
 nez = 1
-numerrin.create_quad_mesh(name, wrapper, corner_points,
+print "to quad create"
+create_quad_mesh(name, wrapper, corner_points,
                           extrude_length, nex, ney, nez)
+#numerrin.create_quad_mesh(name, wrapper, corner_points,
+#                          extrude_length, nex, ney, nez)
 mesh_inside_wrapper = wrapper.get_dataset(name)
 
 wrapper.run()
-print "Steps taken ", mesh_inside_wrapper._time
+print "Run up to time: ", mesh_inside_wrapper._time
